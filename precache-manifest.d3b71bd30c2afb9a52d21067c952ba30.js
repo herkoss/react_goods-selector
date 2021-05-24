faf589150f8c0081ccf85adf86e4b43bb1d9c49b
herkoss <dmytroherko@icloud.com>
2021-05-24 18:41:03 +0300

    Updates

---
+++ precache-manifest.d3b71bd30c2afb9a52d21067c952ba30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbcfe4e0c98732f0d097cd64ee01bc26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4127b93411edca8e4285",
+    "url": "./static/css/main.544d6a23.chunk.css"
+  },
+  {
+    "revision": "567e3ee8d342539f6331",
+    "url": "./static/js/2.977ecf36.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.977ecf36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4127b93411edca8e4285",
+    "url": "./static/js/main.56dd8eab.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
